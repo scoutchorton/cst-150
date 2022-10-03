58f0aa8e55ef28e379d1fee21756e6a18423e901
Caleb Horton <scoutchorton@scoutchorton.io>
2022-10-02 22:55:38 -0400

    Finished with display/file output

--- Activity5/Stats.Designer.cs
+++ Activity5/Stats.Designer.cs
@@ -24,21 +24,42 @@
         /// </summary>
         private void InitializeComponent() {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.statsDisplay = new System.Windows.Forms.TextBox();
             this.openFile = new System.Windows.Forms.OpenFileDialog();
             this.saveFile = new System.Windows.Forms.SaveFileDialog();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.statsDisplay);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(20);
             this.panel1.Size = new System.Drawing.Size(763, 526);
             this.panel1.TabIndex = 0;
+            // 
+            // statsDisplay
+            // 
+            this.statsDisplay.BackColor = System.Drawing.Color.Gray;
+            this.statsDisplay.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.statsDisplay.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.statsDisplay.Location = new System.Drawing.Point(20, 20);
+            this.statsDisplay.Multiline = true;
+            this.statsDisplay.Name = "statsDisplay";
+            this.statsDisplay.ReadOnly = true;
+            this.statsDisplay.Size = new System.Drawing.Size(723, 486);
+            this.statsDisplay.TabIndex = 0;
             // 
             // openFile
             // 
             this.openFile.FileName = "textfile";
+            this.openFile.Title = "Open File for Processing";
+            // 
+            // saveFile
+            // 
+            this.saveFile.Title = "Save Stats File";
             // 
             // main
             // 
@@ -50,6 +71,9 @@
             this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.Name = "main";
             this.Text = "Text Stats";
+            this.TopMost = true;
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -59,5 +83,6 @@
         private Panel panel1;
         private OpenFileDialog openFile;
         private SaveFileDialog saveFile;
+        private TextBox statsDisplay;
     }
 }
--- Activity5/Stats.cs
+++ Activity5/Stats.cs
@@ -9,21 +9,29 @@ namespace Activity5 {
         int vowelCount = 0;
 
         public main() {
+            string stats = "";
             InitializeComponent();
 
             // Prompt to get the file for opening and check for a file
             openFile.ShowDialog();
-            if(openFile.FileName.Length == 0) return;
+            if(openFile.FileName.Length == 0)
+                Close();
 
             // Get the stats of the file
             parseFile(openFile.FileName);
-
-            MessageBox.Show(String.Concat("First: ", first));
-            MessageBox.Show(String.Concat("Last: ", last));
-            MessageBox.Show(String.Concat("Longest: ", longest));
-            MessageBox.Show(String.Concat("Most vowels: ", mostVowels));
+            stats += String.Concat("First: ", first, Environment.NewLine);
+            stats += String.Concat("Last: ", last, Environment.NewLine);
+            stats += String.Concat("Longest: ", longest, Environment.NewLine);
+            stats += String.Concat("Most vowels: ", mostVowels, Environment.NewLine);
 
             // Display stats in the window and write stats to the file
+            saveFile.FileName = Path.ChangeExtension(openFile.FileName, ".stats" + Path.GetExtension(openFile.FileName));
+            saveFile.ShowDialog();
+            if(saveFile.FileName.Length == 0)
+                Close();
+
+            File.WriteAllText(saveFile.FileName, stats);
+            statsDisplay.AppendText(stats);
         }
 
         public void parseFile(string filename) {
@@ -31,7 +39,7 @@ namespace Activity5 {
             string fileData = "";
             string word;
 
-            // Read file line by line (avoids issues with line endings)
+            // Read file line by line (avoids issues with line endings and File.ReadAllText)
             foreach(string line in lines) {
                 fileData += line;
                 fileData += " ";
@@ -40,13 +48,11 @@ namespace Activity5 {
 
             // Grab list of words
             for(int index = 0; index < rawWords.Count(); index++) {
-                word = rawWords[index];
-
-                string lowerWord = word.ToLower();
+                word = rawWords[index].ToLower();
 
                 // Add element to word list if not found
-                if(words.IndexOf(lowerWord) == -1)
-                    words.Add(lowerWord);
+                if(words.IndexOf(word) == -1)
+                    words.Add(word);
             }
 
             // Parse words
@@ -76,6 +82,7 @@ namespace Activity5 {
             char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
             int count = 0;
 
+            // Count each character for being a vowel
             foreach(char character in word) {
                 if(Array.IndexOf(vowels, character) != -1)
                     count++;
